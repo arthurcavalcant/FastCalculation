aea0a6423e2f5012bd762121548f792f839af2fa
Arthur Cavalcante <arthur.ferreira@ccc.ufcg.edu.br>
2025-09-05 21:16:23 -0300

    Add fragment

--- app/src/main/java/br/edu/ifsp/scl/sdm/fastcalculation/GameFragment.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/fastcalculation/GameFragment.kt
@@ -7,7 +7,6 @@ import android.os.Message
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
-import android.view.View.GONE
 import android.view.View.OnClickListener
 import android.view.ViewGroup
 import android.widget.Button
@@ -79,16 +78,8 @@ class GameFragment : Fragment() {
             startRoundTime = System.currentTimeMillis()
             roundDeadlineHandler.sendEmptyMessageDelayed(MSG_ROUND_DEADLINE, settings.roundInterval)
         } else {
-            with(fragmentGameBinding) {
-                roundTv.text = getString(R.string.points)
-                val points = hits * 10f / (totalGameTime / 1000L)
-                "%.1f".format(points).also {
-                    questionTv.text = it
-                }
-                alternativeOneBt.visibility = GONE
-                alternativeTwoBt.visibility = GONE
-                alternativeThreeBt.visibility = GONE
-            }
+            val points = hits * 10f / (totalGameTime / 1000f)
+            (context as OnFinishGame).onFinishGame(points = points)
         }
     }
 
